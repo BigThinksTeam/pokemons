99efbfb311fb3f453901b70f93faac61375a0fb6
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2020-10-04 21:43:13 -0500

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use MongoDB\Driver\Session;
+use App\Http\Requests\RegisterSave;
 
 class AuthController extends Controller
 {
